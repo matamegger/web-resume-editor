a75e60c5b1beeb8659d1dbc60d453f505de8e7e4
Peter Pfaffeneder <peter.pfaffeneder@gmail.com>
2017-06-13 15:52:11 +0200

    Added vuejs

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,6 @@
-var uploadOptions = {
-    serverPath: 'https://api.imgur.com/3/image',
-    fileFieldName: 'image',
-    headers: { 'Authorization': 'Client-ID 9e57cb1c4791cea' },
-    urlPropertyName: 'data.link'
-};
-/** Default editor configuration **/
-$('#editor')
-    .trumbowyg({
-        btns: [
-            ['viewHTML'],
-            ['p', 'blockquote', 'h1', 'h2', 'h3', 'h4'],
-            ['strong', 'em', 'underline', 'del'],
-            ['superscript', 'subscript'],
-            ['createLink', 'unlink'],
-            ['insertImage'],
-            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-            ['unorderedList', 'orderedList'],
-            ['horizontalRule'],
-            ['removeformat'],
-
-            ['upload', 'base64', 'noembed'],
-            ['foreColor', 'backColor'],
-            ['preformatted'],
-            ['fullscreen', 'close']
-        ],
-        plugins: {
-            upload: uploadOptions
-        }
-    });
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World!'
+    }
+});
